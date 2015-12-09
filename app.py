7ffe9abc331a0c6de3ea6bbbad581b4137fe9039
charmelonlai <charles.lai@berkeley.edu>
2015-12-09 14:52:38 -0800

    working datg database

--- app.py
+++ app.py
@@ -35,11 +35,31 @@ def mapview():
 		identifier="sndmap",
 		lat= 39.5,
 		lng= -98.35,
-		zoom = 5,
+		zoom = 3,
 		style = "height: 100%; width: 100%; top:0; left:0; position:absolute; z-index:200",
 		markers= {'http://maps.google.com/mapfiles/ms/icons/green-dot.png': mapData}
 	)
+
+	urlData = request.form.get('url')
+	if urlData not in db:
+		db[urlData] = render_template('map.html', sndmap=sndmap)
+
 	return render_template('map.html', sndmap=sndmap)
+
+@app.route("/<url>", methods=['GET'])
+def load_redirect(url):
+    """
+    Redirect the request to the URL associated =short=, otherwise return 404
+    NOT FOUND
+    """
+    print url
+
+    if url in db:
+        return db[url]
+    else:
+        #return render_template('404.html'),404
+        abort(404)
+
 
 if __name__ == "__main__":
     app.run()
